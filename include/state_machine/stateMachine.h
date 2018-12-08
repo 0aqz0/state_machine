ad738805d9e413f4c3ced2c49d729cb6cc0c4d5b
0aqz0 <18026332256@163.com>
2018-12-08 22:24:22 +0800

    update

--- include/state_machine/stateMachine.h
+++ include/state_machine/stateMachine.h
@@ -1,16 +1,16 @@
 namespace decision{
 enum State
 {
-	ATTACK = 0;    // attack
-    DEFENCE = 1;   // defence
-    SPY = 2;       // spy 
-    UNKOWN = 3;    // unkown
-}
+	ATTACK  = 0,      // attack
+    DEFENCE = 1,      // defence
+    SPY     = 2,      // spy 
+    UNKOWN  = 3,      // unkown
+};
 enum Event
 {
-	ATTACKED = 0;  // be attacked
-	ATTACKING = 1; // attacking
-	NOTHING = 2;   // nothing	
+	ATTACKED  = 0,    // be attacked
+	ATTACKING = 1,    // attacking
+	NOTHING   = 2,    // nothing	
 };
 class StateMachine
 {
@@ -20,5 +20,5 @@ public:
 private:
     State state;
     Event event;
-}
+};
 }
--- src/state_machine_node.cpp
+++ src/state_machine_node.cpp
@@ -1,5 +1,5 @@
 #include <ros/ros.h>
-
+#include "state_machine/stateMachine.h"
 
 
 int main(int argc, char** argv)
