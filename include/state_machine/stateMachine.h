f3114498de304ea8c7835b3293acebf80a2da9f6
0aqz0 <18026332256@163.com>
2018-12-08 22:37:12 +0800

    update states and events

--- include/state_machine/stateMachine.h
+++ include/state_machine/stateMachine.h
@@ -3,14 +3,15 @@ enum State
 {
 	ATTACK  = 0,      // attack
     DEFENCE = 1,      // defence
-    SPY     = 2,      // spy 
+    SPY     = 2,      // spy on enemy
     UNKOWN  = 3,      // unkown
 };
 enum Event
 {
-	ATTACKED  = 0,    // be attacked
-	ATTACKING = 1,    // attacking
-	NOTHING   = 2,    // nothing	
+	ATTACKED   = 0,    // be attacked           ------------defence
+	ATTACKING  = 1,    // attacking             ------------attack
+	NOTHING    = 2,    // nothing happened      ------------unkown
+	FIND_ENEMY = 3,	   // find enemy            ------------spy on enemy
 };
 class StateMachine
 {
